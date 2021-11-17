d599b7d58514c82e25b2ef114cfc476d486aec1b
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-11-17 23:15:15 +0100

    HomeLayout

--- src/ui/layouts/HomeLayout.vue
+++ src/ui/layouts/HomeLayout.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    Home Layout
-  </div>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <div class="container">
+      <slot name="content"></slot>
+    </div>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
 </template>
 
 <script>
-
 export default {
-  name: 'HomeLayout',
-}
+  name: "HomeLayout",
+};
 </script>
 
-<style>
-</style>
+<style></style>
--- src/ui/pages/Home.vue
+++ src/ui/pages/Home.vue
@@ -1,14 +1,27 @@
 <template>
-  <h1>{{ title }}</h1>
-  <ProductList />
+  <HomeLayout>
+    <template v-slot:header>
+      <h1>{{ title }}</h1>
+    </template>
+
+    <template v-slot:content>
+      <ProductList />
+    </template>
+
+    <template v-slot:footer>
+      <p>Here's some footer info</p>
+    </template>
+  </HomeLayout>
 </template>
 
 <script>
+import HomeLayout from "../layouts/HomeLayout.vue";
 import ProductList from "../components/ProductList.vue";
 
 export default {
   name: "Home",
   components: {
+    HomeLayout,
     ProductList,
   },
   data() {
