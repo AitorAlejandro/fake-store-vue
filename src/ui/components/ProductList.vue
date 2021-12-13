4d840566bc52a91e023161a14b062347027b869e
AitorAlejandro <aitor.alejandro.herrera@gmail.com>
2021-12-14 00:55:43 +0100

    adding styles to ProductList

--- src/ui/components/ProductList.vue
+++ src/ui/components/ProductList.vue
@@ -1,11 +1,15 @@
 <template>
-  <ul v-cloak v-if="products.length > 0">
-    <li v-for="product in products" :key="product.id">
-      <div>{{ product.title }}</div>
-      <div>{{ product.price }}</div>
-      <div>{{ product.description }}</div>
-      <div>{{ product.category }}</div>
+  <ul v-cloak v-if="products.length > 0" class="product-list">
+    <li
+      v-for="product in products"
+      :key="product.id"
+      class="product-list__item"
+    >
       <img alt="" :src="product.image" />
+      <div class="product-list__item-content">
+        <div class="product-list__item-title">{{ product.title }}</div>
+        <div>{{ product.price }}</div>
+      </div>
     </li>
   </ul>
   <div v-else>Loading ...</div>
@@ -36,4 +40,25 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.product-list {
+  list-style-type: none;
+  margin: 0;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-gap: 3rem;
+}
+
+.product-list__item {
+  border-radius: 4px;
+  box-shadow: 0px 0px 12px -3px rgba(0, 0, 0, 0.4);
+}
+
+.product-list__item-content {
+  padding: 2rem;
+}
+
+.product-list__item-title {
+  font-size: 2rem;
+}
+</style>
